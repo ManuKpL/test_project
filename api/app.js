d0304adbc7eb20ff9caedbcb911c5d5b809b821c
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-08-16 14:27:00 +0200

    reorganized front into app and instantiated angular app

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ var app = express();
 
 var rootRouter = require('./routes/root')
 
-app.use(express.static('public'));
+app.use(express.static('app'));
 app.use('/', rootRouter);
 
 app.listen(3000, function () {
