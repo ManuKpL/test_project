2fb6e4e3867b21d45b29b2be034029dbd20c5824
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-08-17 13:24:20 +0200

    reorganized since router is not necessary for the only html rendering of backend

--- api/app.js
+++ api/app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 
-var rootRouter = require('./routes/root')
-
 app.use(express.static('app'));
-app.use('/', rootRouter);
+app.use('/bower_components', express.static('bower_components'));
+
+app.get('/',function (req, res) { res.sendFile('index.html') });
 
 app.listen(3000, function () {
-  console.log("Server started, listening to port 3000... \n")
+  console.log("\nServer started, listening to port 3000...\n")
+  console.log("\nsee: http://localhost:3000/\n")
 });
--- api/routes/root.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.route('/')
-  .get(function (req, res) {
-    res.sendFile('index.html');
-  });
-
-module.exports = router;
