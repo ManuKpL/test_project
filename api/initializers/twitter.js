da751378afdad378f6251de9bd408d41c6ecb9ef
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-08-21 22:02:25 +0200

    added env variables and required ntwitter module in separate file

---
+++ api/initializers/twitter.js
@@ -0,0 +1,9 @@
+var twitter = require("ntwitter");
+module.exports = new twitter({
+        consumer_key: process.env.YOUR_CONSUMER_KEY,
+        consumer_secret: process.env.YOUR_CONSUMER_SECRET,
+        access_token_key: process.env.USER_ACCESS_TOKEN,
+        access_token_secret: process.env.USER_ACCESS_TOKEN_SECRET
+    }),
+    stream = null
+    users = [];
