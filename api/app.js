49c1d7c600c411b74dab14e1e80224ebf96c61fe
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-08-21 11:16:23 +0200

    intalled ntwitter module

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 require('dotenv').config({silent: true});
 
 app.use(express.static('app'));
@@ -7,7 +8,7 @@ app.use('/bower_components', express.static('bower_components'));
 
 app.get('/',function (req, res) { res.sendFile('index.html') });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("\nServer started, listening to port 3000...\n")
   console.log("\nsee: http://localhost:3000/\n")
 });
