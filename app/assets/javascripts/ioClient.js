7b68774242e0cc59a350dad8588539b5b20f87c1
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-08-24 13:33:43 +0200

    removed test console.log

--- app/assets/javascripts/ioClient.js
+++ app/assets/javascripts/ioClient.js
@@ -1,11 +1,7 @@
 var client = io.connect('http://localhost:3000')
 client.on('hello', function(data) {
-  console.log("HELLO " + data.hello);
   client.emit('start stream');
 });
-client.on('bye', function(data) {
-  console.log("BYE " + data.bye)
-});
 client.on('data', function(data) {
-  console.log(data)
+  console.log(data);
 });
