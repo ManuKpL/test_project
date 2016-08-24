de1c361826f5d4726d0a884603af6a0295cb379a
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-08-24 11:36:00 +0200

    added twitter stream creation and destruction on first client creation

--- api/app.js
+++ api/app.js
@@ -4,16 +4,57 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var port = process.env.PORT || 3000;
 
-require('dotenv').config({silent: true});
-
-var tw = require('./initializers/twitter')
-
 app.use(express.static('app'));
-app.use('/bower_components', express.static('bower_components'));
+app.use(express.static('bower_components'));
 
 app.get('/',function (req, res) { res.sendFile('index.html') });
 
 server.listen(port, function () {
   console.log("\nServer started, listening to port 3000...\n")
   console.log("\nsee: http://localhost:3000/\n")
+});
+
+
+require('dotenv').config({silent: true});
+
+var tw = require('./initializers/twitter');
+var stream = null;
+var track = "paris";
+var users = [];
+
+io.on('connection', function (client) {
+  client.emit('hello', { hello: 'world' });
+  if (users.indexOf(client.id) === -1) {
+    users.push(client.id);
+  }
+  console.log(users.length + ' client(s) connected');
+
+  client.on('start stream', function () {
+    if (stream === null) {
+      tw.stream('statuses/filter', { track: track }, function (twStream) {
+        console.log('Stream started');
+        stream = twStream;
+        stream.on('data', function (data) {
+          if (users.length > 0) {
+            client.broadcast.emit('data', data);
+            client.emit('data', data);
+          } else {
+            stream.destroy();
+            stream = null;
+          }
+        });
+        stream.on('destroy', function (response) {
+          console.log('Stream ended');
+        });
+      });
+    }
+  });
+
+  client.on('disconnect', function () {
+    var index = users.indexOf(client.id);
+    if (index !== -1) {
+      users.splice(index, 1);
+    }
+    console.log(users.length + ' client(s) connected');
+  });
 });
