d813319b55e3fe8bb5290b84520827ab7217de34
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2016-08-29 11:23:07 +0200

    added the counter to the pages controller

--- app/pages/pagesController.js
+++ app/pages/pagesController.js
@@ -1,13 +1,16 @@
 testApp.controller('pagesController', function ($scope, client) {
+  $scope.counter = 0;
   client.on('hello', function (location) {
     client.emit('start stream', location);
   });
   client.on('precise tweet', function(data) {
+    $scope.counter++;
     var coordinates = new google.maps.LatLng(data.lng,data.lat);
     // var worldPreciseTweets defined in map.js
     worldPreciseTweets.push(coordinates);
   });
   client.on('average tweet', function(data) {
+    $scope.counter++;
     var coordinates = new google.maps.LatLng(data.lng,data.lat);
     // var worldAverageTweets defined in map.js
     worldAverageTweets.push(coordinates);
